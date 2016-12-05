fcdadd2dd95cfe873e574044aecb7feb99a8b6db
Yuan He <yuanthehe@gmail.com>
2016-12-05 15:38:29 -0500

    Rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205202038) do
+ActiveRecord::Schema.define(version: 20161205203119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
