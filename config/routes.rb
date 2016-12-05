ba424f385ae5bbb49154229fc205e67a468a37e8
Yuan He <yuanthehe@gmail.com>
2016-12-05 15:22:41 -0500

    Generating User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
+  get 'user/create'
+
+  get 'user/update'
+
+  get 'user/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
