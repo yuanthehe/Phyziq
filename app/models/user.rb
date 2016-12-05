a5d9d6ac1a5370585edb8ce52dc8787c54cfd707
Yuan He <yuanthehe@gmail.com>
2016-12-05 17:27:20 -0500

    Set up user _form validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :trainers, through: :appointments, source: :user
   has_many :trainees, through: :appointments, source: :user
+
+  validates :name, :email, :address, presence: true
+
+  has_secure_password
 end
