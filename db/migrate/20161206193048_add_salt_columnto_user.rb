198a3a31ace1cc87720a4fabc0a4f9ac274d844d
Daniel Cheley <danielcheley@hotmail.com>
2016-12-06 16:03:30 -0500

    Deleting user files

--- db/migrate/20161206193048_add_salt_columnto_user.rb
+++
@@ -1,5 +0,0 @@
-class AddSaltColumntoUser < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :salt, :string
-  end
-end
--- db/migrate/20161206193258_add_crypted_columnto_users.rb
+++
@@ -1,5 +0,0 @@
-class AddCryptedColumntoUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :crypted_password, :string
-  end
-end
