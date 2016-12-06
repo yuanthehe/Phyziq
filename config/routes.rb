692cc581152cce676fa47aabfe24941a826dfdd4
Daniel Cheley <danielcheley@hotmail.com>
2016-12-06 16:20:15 -0500

    Readd routes and users _form

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/show'
-
-  get 'users/edit'
+  root "users#index"
 
   resources :users
   resources :appointments
+  resources :user_sessions, only [:new, :create, :destroy]
+
+  get 'login' => 'user_sessions#new', :as => :login
+  get 'logout' => 'user_sessions#destroy', as: => :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
