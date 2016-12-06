7171122ab854ef1c2f76c73001f861a6413e8477
Daniel Cheley <danielcheley@hotmail.com>
2016-12-06 18:40:45 -0500

    Adding appointment controller methods, creating trainer/trainee partials and starting user show page

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,13 +1,23 @@
 class AppointmentsController < ApplicationController
-  def new
-  end
+  before_action :require_login
+  # before_action :require_trainer only: [:destroy]
 
   def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.user = current_user
+
+    if @appointment.save
+      @appointment.trainee_id = @appointment.user.id
+      redirect_to user_url(@appointment.user), notice: "Appointment request sent!"
+    else
+      flash[:notice] = "Failed to process appointment request"
+      render '/users'
+    end
   end
 
-  def edit
-  end
+  private
 
-  def destroy
+  def pledge_params
+    params.require(:appointment).permit(:time)
   end
 end
