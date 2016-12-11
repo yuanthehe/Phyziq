3741a1efb1698aa57e5955f1ce471ac88c8bcee1
Daniel Cheley <danielcheley@hotmail.com>
2016-12-11 14:46:14 -0500

    Fixing login feature and indenting user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ApplicationRecord
   validates :name, :email, :address, presence: true
   validates :email, uniqueness: true
 
-	validates :password, length: { minimum: 8 }, on: :create
-	validates :password, confirmation: true, on: :create
-	validates :password_confirmation, presence: true, on: :create
+	  validates :password, length: { minimum: 8 }, on: :create
+	  validates :password, confirmation: true, on: :create
+	  validates :password_confirmation, presence: true, on: :create
 
   accepts_nested_attributes_for :authentications
 
