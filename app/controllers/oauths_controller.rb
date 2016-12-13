528e3619e44e4d65198255d7d734d418a2f9ed59
Daniel Cheley <danielcheley@hotmail.com>
2016-12-12 23:16:11 -0500

    Refactoring callback method, changing logout route and removing password fields from user partial

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,5 +1,4 @@
 class OauthsController < ApplicationController
-  #skip_before_filter :require_login
   before_action :require_login, only: :destroy
   # sends the user on a trip to the provider,
   # and after authorizing there back to the callback url.
@@ -9,38 +8,31 @@ class OauthsController < ApplicationController
   # this is where all of the magic happens
   def callback
     provider = params[:provider]
-    
+
     if @user = login_from(:google)
-      # user has already linked their account with google
-      # @user.google_access_token = @access_token.token
-      flash[:alert] = "Logged in using #{provider.titleize}!"
-      redirect_to root_path
-    else
-      # User has not linked their account with Github yet. If they are logged in,
+       # user has already linked their account with google
+       # @user.google_access_token = @access_token.token
+       flash[:alert] = "Logged in from Google!"
+       redirect_to root_path
+    # else
+      # User has not linked their account with Google yet. If they are logged in,
       # authorize the account to be linked. If they are not logged in, require them
       # to sign in. NOTE: If you wanted to allow the user to register using oauth,
       # this section will need to be changed to be more like the wiki page that was
       # linked earlier.
       # if logged_in?
-      #   # link_account(:google)
-      #   link_account(provider)
-      #   flash[:notice] = "Account linked from #{provider.titleize}!"
+      #   link_account(:google)
+      #   flash[:notice] = "Account linked from Google!"
       #   redirect_to user_path(@user)
-      # else
-      @user = create_from(:google)
-      auto_login(@user)
-      flash[:alert] = 'Google account successfully linked.'
-      redirect_to user_url(@user)
-      # end
+    else
+        @user = create_from(:google)
+        reset_session # protect from session fixation attack
+        auto_login(@user)
+        flash[:alert] = 'Google account successfully linked!'
+        redirect_to user_url(@user)
     end
   end
-  # This is used to allow users to unlink their account from the oauth provider.
-  #
-  # In order to use this action you will need to include this route in your routes file:
-  # delete "oauth/:provider" => "oauths#destroy", :as => :delete_oauth
-  #
-  # You will need to provide a 'provider' parameter to the action, create a link like this:
-  # link_to 'unlink', delete_oauth_path('github'), method: :delete
+
   def destroy
     provider = params[:provider]
 
@@ -51,23 +43,23 @@ class OauthsController < ApplicationController
     else
       flash[:alert] = "You do not currently have a linked #{provider.titleize} account."
     end
-    logout
     redirect_to root_path
   end
 end
-  # private
-  # def link_account(provider)
-  #   if @user = add_provider_to_user(provider)
-  #     If you want to store the user's Github login, which is required in order to interact with their Github account, uncomment the next line.
-  #     You will also need to add a 'github_login' string column to the users table.
-  #
-  #     @user.update_attribute(:google_login, @user_hash[:user_info]['login'])
-  #     flash[:notice] = "You have successfully linked your Google account."
-  #   else
-  #     flash[:alert] = "There was a problem linking your Google account."
-  #   end
-  # end
 
+#   private
+#   def link_account(provider)
+#     if @user = add_provider_to_user(provider)
+#       If you want to store the user's Github login, which is required in order to interact with their Github account, uncomment the next line.
+#       You will also need to add a 'github_login' string column to the users table.
+#
+#       @user.update_attribute(:google_login, @user_hash[:user_info]['login'])
+#       flash[:notice] = "You have successfully linked your Google account."
+#     else
+#       flash[:alert] = "There was a problem linking your Google account."
+#     end
+#   end
+#
 #   def auth_params
 #     params.permit(:provider, :code)
 #   end
