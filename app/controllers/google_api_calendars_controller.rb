d0f682c5eaf66a076fc46b24a3f28d378fe51778
Daniel Cheley <danielcheley@hotmail.com>
2016-12-13 00:14:33 -0500

    Implementing callback method

--- app/controllers/google_api_calendars_controller.rb
+++ app/controllers/google_api_calendars_controller.rb
@@ -13,6 +13,16 @@ class GoogleApiCalendarsController < ApplicationController
   end
 
   def callback
+    client = Signet::OAuth2::Client.new({
+      client_id: ENV.fetch('GOOGLE_API_CLIENT_ID'),
+      client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET'),
+      token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+      redirect_uri: url_for(:action => :callback),
+      code: params[:code]
+    })
+    response = client.fetch_access_token!
+    session[:access_token] = response['access_token']
+    redirect_to url_for(:action => :calendars)
   end
 
   def calendars
