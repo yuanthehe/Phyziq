c010a8be281be3636f00e745bf0a676d1b7cd1fc
Yuan He <yuanthehe@gmail.com>
2016-12-22 13:23:50 -0500

    Adding timeslot arrays to the user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
 
   def show
     # @user = User.find(params[:id])
-    @weekly_event_list = weekly_event_list
+    @weekly_event_list = hourly_event_list
   end
 
   def edit
@@ -71,36 +71,52 @@ class UsersController < ApplicationController
        service = Google::Apis::CalendarV3::CalendarService.new
        service.authorization = client
 
-       result = service.list_events('primary')
-         weekly = result.items.map {|e|
+      result = service.list_events('primary')
+      weekly = result.items.map {|e|
            e.start.date
          }.compact
-           next_six_days.map {|day|
-             if weekly.include?(day)
-               next "Busy on #{day}"
-             else
-               next "Free on #{day}"
-             end
-           }
+
+      hourly = result.items.map {|e|
+           e.start.date_time.to_i
+         }.compact
+
+      next_six_days.map {|day|
+        if weekly.include?(day)
+          next "Busy on #{day}"
+        else
+          next "Free on #{day}"
+        end
+        }
   end
 
   def hourly_event_list
-    #Should be redirected here from weekly_event_list
-    #Loop through time_slots array to check for available time_slots
-    #Display time slots that do not include e.start.date_time("%H")
-    time_slots = ["10","12","14","16","18"]
+    client = Signet::OAuth2::Client.new({
+    client_id: "#{Rails.application.secrets.sorcery_google_key}",
+    client_secret: "#{Rails.application.secrets.sorcery_google_secret}",
+    token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+    access_token: session[:access_token]
+    })
+    client.expires_in = Time.now + 1_000_000
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.authorization = client
 
-    hourly_info = result.items.map { |e|
-    if e.start.date_time.instance_of?(DateTime)
-      puts "looooop"
-      d = e.start.date_time.strftime("%H")
-      if time_slots.include?(d)
-        next time_slots
+    result = service.list_events('primary')
+    hourly = result.items.map {|e|
+            if e.start.date_time != nil
+              e.start.date_time.to_i
+            else
+              next
+            end
+          }.compact
+
+       time_slots = ['9', '10', '11', '12', '13', '14', '15', '16', '17' ]
+    time_slots.map {|hour|
+      if daily.include?(hour)
+        next "Busy"
       else
-        next time_slots
+        next "Free"
       end
-    end
-    }
+      }
   end
 
   def calendars
@@ -127,11 +143,36 @@ private
   end
 
   def next_six_days
-    today = Date.today
-    (today .. today + 6).map {|date| "#{date}"}
+    tomorrow = Date.today + 1
+    (tomorrow .. tomorrow + 6).map {|date| "#{date}"}
   end
 
   def available_hours
+    d_1 = Date.today + 1
+    d_2 = Date.today + 2
+    d_3 = Date.today + 3
+    d_4 = Date.today + 4
+    d_5 = Date.today + 5
+    d_6 = Date.today + 6
+    d_7 = Date.today + 7
 
+    t_1 = Time.parse("09:00").seconds_since_midnight.seconds
+    t_2 = Time.parse("10:00").seconds_since_midnight.seconds
+    t_3 = Time.parse("11:00").seconds_since_midnight.seconds
+    t_4 = Time.parse("12:00").seconds_since_midnight.seconds
+    t_5 = Time.parse("13:00").seconds_since_midnight.seconds
+    t_6 = Time.parse("14:00").seconds_since_midnight.seconds
+    t_7 = Time.parse("15:00").seconds_since_midnight.seconds
+    t_8 = Time.parse("16:00").seconds_since_midnight.seconds
+    t_9 = Time.parse("17:00").seconds_since_midnight.seconds
+    t_10 = Time.parse("18:00").seconds_since_midnight.seconds
+
+    day_1 = [(d_1 + t_1).to_i, (d_1 + t_2).to_i, (d_1 + t_3).to_i, (d_1 + t_4).to_i, (d_1 + t_5).to_i, (d_1 + t_6).to_i ,(d_1 + t_7).to_i, (d_1 + t_8).to_i, (d_1 + t_9).to_i ,(d_1 + t_10).to_i]
+    day_2 = [(d_2 + t_1).to_i, (d_2 + t_2).to_i, (d_2 + t_3).to_i, (d_2 + t_4).to_i, (d_2 + t_5).to_i, (d_2 + t_6).to_i ,(d_2 + t_7).to_i, (d_2 + t_8).to_i, (d_2 + t_9).to_i ,(d_2 + t_10).to_i]
+    day_3 = [(d_3 + t_1).to_i, (d_3 + t_2).to_i, (d_3 + t_3).to_i, (d_3 + t_4).to_i, (d_3 + t_5).to_i, (d_3 + t_6).to_i ,(d_3 + t_7).to_i, (d_3 + t_8).to_i, (d_3 + t_9).to_i ,(d_3 + t_10).to_i]
+    day_4 = [(d_4 + t_1).to_i, (d_4 + t_2).to_i, (d_4 + t_3).to_i, (d_4 + t_4).to_i, (d_4 + t_5).to_i, (d_4 + t_6).to_i ,(d_4 + t_7).to_i, (d_4 + t_8).to_i, (d_4 + t_9).to_i ,(d_4 + t_10).to_i]
+    day_5 = [(d_5 + t_1).to_i, (d_5 + t_2).to_i, (d_5 + t_3).to_i, (d_5 + t_4).to_i, (d_5 + t_5).to_i, (d_5 + t_6).to_i ,(d_5 + t_7).to_i, (d_5 + t_8).to_i, (d_5 + t_9).to_i ,(d_5 + t_10).to_i]
+    day_6 = [(d_6 + t_1).to_i, (d_6 + t_2).to_i, (d_6 + t_3).to_i, (d_6 + t_4).to_i, (d_6 + t_5).to_i, (d_6 + t_6).to_i ,(d_6 + t_7).to_i, (d_6 + t_8).to_i, (d_6 + t_9).to_i ,(d_6 + t_10).to_i]
+    day_7 = [(d_7 + t_1).to_i, (d_7 + t_2).to_i, (d_7 + t_3).to_i, (d_7 + t_4).to_i, (d_7 + t_5).to_i, (d_7 + t_6).to_i ,(d_7 + t_7).to_i, (d_7 + t_8).to_i, (d_7 + t_9).to_i ,(d_7 + t_10).to_i]
   end
 end
