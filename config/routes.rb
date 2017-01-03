7145fea3456bd4d9706a4a5db4f1b7a171072fc4
Daniel Cheley <danielcheley@hotmail.com>
2017-01-03 14:09:28 -0500

    Creating day availability routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,17 @@ Rails.application.routes.draw do
   get 'login' => 'user_sessions#new', :as => :login
   get 'logout' => 'user_sessions#destroy', :as => :logout
 
-
   post "oauth2callback" => "oauths#callback"
-  get "oauth2callback" => "oauths#callback" # for use with Github
+  get "oauth2callback" => "oauths#callback"
   get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
   delete "oauth/:provider" => "oauths#destroy", :as => :delete_oauth
 
-  # get "https://www.googleapis.com/calendar/v3/calendars/calendarId/events"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "day_1" => "user#day_1"
+  get "day_2" => "user#day_2"
+  get "day_3" => "user#day_3"
+  get "day_4" => "user#day_4"
+  get "day_5" => "user#day_5"
+  get "day_6" => "user#day_6"
+  get "day_7" => "user#day_7"
+
 end
