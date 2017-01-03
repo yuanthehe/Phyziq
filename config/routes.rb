71a3270a1a3777b4265bca64aa736928584f2123
Daniel Cheley <danielcheley@hotmail.com>
2017-01-03 16:01:42 -0500

    Changing routes based on appointment id

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users
-  resources :appointments
+  # resources :appointments
   resources :user_sessions, only: [:new, :create, :destroy]
 
   get 'login' => 'user_sessions#new', :as => :login
@@ -13,12 +13,12 @@ Rails.application.routes.draw do
   get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
   delete "oauth/:provider" => "oauths#destroy", :as => :delete_oauth
 
-  get "day_1" => "users/#{:user_id}/day_1", :as => :day_1
-  get "day_2" => "users/#{:user_id}/day_2", :as => :day_2
-  get "day_3" => "users/#{:user_id}/day_3", :as => :day_3
-  get "day_4" => "users/#{:user_id}/day_4", :as => :day_4
-  get "day_5" => "users/#{:user_id}/day_5", :as => :day_5
-  get "day_6" => "users/#{:user_id}/day_6", :as => :day_6
-  get "day_7" => "users/#{:user_id}/day_7", :as => :day_7
+  get "day_1" => "#{:appointment_id}/day_1", :as => :day_1
+  get "day_2" => "#{:appointment_id}/day_2", :as => :day_2
+  get "day_3" => "#{:appointment_id}/day_3", :as => :day_3
+  get "day_4" => "#{:appointment_id}/day_4", :as => :day_4
+  get "day_5" => "#{:appointment_id}/day_5", :as => :day_5
+  get "day_6" => "#{:appointment_id}/day_6", :as => :day_6
+  get "day_7" => "#{:appointment_id}/day_7", :as => :day_7
 
 end
