6c8cead30c53bf722cf7b220e330a619c0d2f153
Daniel Cheley <danielcheley@hotmail.com>
2017-01-03 15:16:12 -0500

    Uncommenting routes and adding symbols

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,12 @@ Rails.application.routes.draw do
   get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
   delete "oauth/:provider" => "oauths#destroy", :as => :delete_oauth
 
-  # get "day_1" => "users/#{:user_id}/day_1", :as => :day_1
-  # get "day_2" => "users/#{@user.id}/day_2"
-  # get "day_3" => "users#day_3"
-  # get "day_4" => "users#day_4"
-  # get "day_5" => "users#day_5"
-  # get "day_6" => "users#day_6"
-  # get "day_7" => "users#day_7"
+  get "day_1" => "users/#{:user_id}/day_1", :as => :day_1
+  get "day_2" => "users/#{:user_id}/day_2", :as => :day_2
+  get "day_3" => "users/#{:user_id}/day_3", :as => :day_3
+  get "day_4" => "users/#{:user_id}/day_4", :as => :day_4
+  get "day_5" => "users/#{:user_id}/day_5", :as => :day_5
+  get "day_6" => "users/#{:user_id}/day_6", :as => :day_6
+  get "day_7" => "users/#{:user_id}/day_7", :as => :day_7
 
 end
