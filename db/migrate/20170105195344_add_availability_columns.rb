f713087a4d80789300b05276d9f202247a5bcefa
Daniel Cheley <danielcheley@hotmail.com>
2017-01-05 15:05:28 -0500

    Fixing migration

--- db/migrate/20170105195344_add_availability_columns.rb
+++ db/migrate/20170105195344_add_availability_columns.rb
@@ -1,11 +1,11 @@
 class AddAvailabilityColumns < ActiveRecord::Migration[5.0]
   def change
-    add_column :availabilities, :day_1, array: true, default: []
-    add_column :availabilities, :day_2, array: true, default: []
-    add_column :availabilities, :day_3, array: true, default: []
-    add_column :availabilities, :day_4, array: true, default: []
-    add_column :availabilities, :day_5, array: true, default: []
-    add_column :availabilities, :day_6, array: true, default: []
-    add_column :availabilities, :day_7, array: true, default: []
+    add_column :availabilities, :day_1, :string, array: true, default: []
+    add_column :availabilities, :day_2, :string, array: true, default: []
+    add_column :availabilities, :day_3, :string, array: true, default: []
+    add_column :availabilities, :day_4, :string, array: true, default: []
+    add_column :availabilities, :day_5, :string, array: true, default: []
+    add_column :availabilities, :day_6, :string, array: true, default: []
+    add_column :availabilities, :day_7, :string, array: true, default: []
   end
 end
