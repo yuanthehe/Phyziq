212bbcb51148896e472ca1bc29ba845246d67452
Daniel Cheley <danielcheley@hotmail.com>
2017-01-12 15:49:25 -0500

    Adding flash alert to refresh_availability

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -25,6 +25,7 @@ class AvailabilitiesController < ApplicationController
       day_7: @availability_day_7,
       user_id: current_user.id
       )
+    flash[:alert] = "Availabilty updated!"
     redirect_to root_path
   end
 
