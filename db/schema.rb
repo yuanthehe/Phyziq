285f2ee35c5ee868ebf4b114495553bdf23c34ae
Yuan He <yuanthehe@gmail.com>
2017-01-13 12:23:26 -0500

    adding bio column to user table, added trainer update profile page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113011009) do
+ActiveRecord::Schema.define(version: 20170113163353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20170113011009) do
     t.string   "address"
     t.boolean  "trainer"
     t.string   "password_digest"
+    t.text     "bio"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
