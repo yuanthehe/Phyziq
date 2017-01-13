a94bc25bb1da733eb23e9e6f40410f41c7ff9611
Yuan He <yuanthehe@gmail.com>
2017-01-12 20:11:05 -0500

    adding name column to categoies model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113003151) do
+ActiveRecord::Schema.define(version: 20170113011009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170113003151) do
     t.datetime "updated_at",  null: false
     t.string   "description"
     t.boolean  "status"
+    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|
