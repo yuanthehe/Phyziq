5941e1476f6c6b5518414224df3c8fb1631f2598
Daniel Cheley <danielcheley@hotmail.com>
2017-01-13 14:55:13 -0500

    Indenting index method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,14 @@ class UsersController < ApplicationController
 
   def index
     @user = User.where(trainer: true)
-      if params[:address]
-        flash[:alert] = "Searched for nearby trainers!"
-        @user = User.near(params[:address], 5, units: :km).order("name ASC")
-      elsif params[:search]
-        @user = User.search(params[:search]).order("name ASC")
-      else
-        @user = User.all
-      end
+    if params[:address]
+      flash[:alert] = "Searched for nearby trainers!"
+      @user = User.near(params[:address], 5, units: :km).order("name ASC")
+    elsif params[:search]
+      @user = User.search(params[:search]).order("name ASC")
+    else
+      @user = User.all
+    end
   end
 
   def create
