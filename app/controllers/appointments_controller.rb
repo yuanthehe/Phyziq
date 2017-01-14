08ac854ab2a5dde3509fec30331b5efccddca4ac
Daniel Cheley <danielcheley@hotmail.com>
2017-01-14 01:25:07 -0500

    Adding trainer address to api insert event call

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -445,6 +445,7 @@ private
    event = Google::Apis::CalendarV3::Event.new({
         'summary':"#{current_user.name}'s Training Session with #{@user.name}",
         'description':'Booked through Phyziq.com',
+        'location':"#{@user.address}"
         'start':{
           'date_time': "#{up}"
         },
