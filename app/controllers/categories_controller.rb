141b5cf805ac11322ae6ce4577258fb329b89779
Daniel Cheley <danielcheley@hotmail.com>
2017-01-15 22:56:38 -0500

    Adding before action to exclude index view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,14 @@
 class CategoriesController < ApplicationController
-  before_action :load_user
-  before_action :load_category
+  before_action :load_user, only: [:show, :edit, :destroy, :update]
+  before_action :load_category, only: [:show, :edit, :destroy, :update]
 
   def new
     @category = @user.category.build
+  end
+
+  def index
+    @user = User.where(trainer: true)
+    @category = @user.category
   end
 
   def create
